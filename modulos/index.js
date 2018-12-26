4b8322959f3769f086028576d0e8e7217f1c5d96
Brayan Santos <v.brayansantos@gmail.com>
2018-12-26 14:39:18 -0200

    first commit

---
+++ modulos/index.js
@@ -0,0 +1,16 @@
+/** Commands
+ * npm login
+ * npm publish
+ * npm version
+            patch "Atualização minimas"
+            minor "Funcionaliddade novas"
+            major "Atualização grandes"
+*/
+
+exports.sum = function (a, b ) {
+    return a + b;
+}
+
+exports.sub = function (a, b ) {
+    return a - b;
+}
